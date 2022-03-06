a9dbde0b58f38e950145a4f5040393519fae18d8
Radek <radek.cihi@entry-cz.com>
2022-03-06 19:50:41 +0100

    chore(package setup): lerna setting up workspaces

--- packages/client/src/components/nav-bar.tsx
+++ packages/client/src/components/nav-bar.tsx
@@ -3,5 +3,5 @@ import * as React from "react";
 export interface INavbarProps {}
 
 export function Navbar(props: INavbarProps) {
-  return <div>Nav bar here!</div>;
+  return <div>Nav bar here!wassup{}</div>;
 }
